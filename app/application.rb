90343b335f6bc6f92353d319e5279ff6cfbd9222
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-25 19:09:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,15 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+    
+    if num_1 == num_2 && num_2 == num_3
+      resp.write
+  
+      
     resp.write "Hello, World"
     resp.finish
   end
